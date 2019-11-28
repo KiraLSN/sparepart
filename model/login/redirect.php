d5b1f69a726fcc921d990f63e8ae43a482a8b561
luciano <lucsoaam@gmail.com>
2019-11-27 22:55:22 -0400

    Mudanca no redirect em teste.

--- model/login/redirect.php
+++ model/login/redirect.php
@@ -2,7 +2,7 @@
 //echo dirname( $_SERVER['PHP_SELF']);
   
 if ( $_SESSION['logado'] != true ) {
-	header('location: ' . dirname( $_SERVER['PHP_SELF'] ) . '/view/login.php');
+	header('location: ..' . dirname( $_SERVER['PHP_SELF'] ) .'/view/login.php');
 }
 
 ?>
